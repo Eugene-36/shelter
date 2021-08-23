58507dcc7dfbd7261db996b43358576235a8139d
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-08-24 01:52:51 +0300

    new info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Check from './components/check/check';
 function App() {
   return (
     <div className="App">
       <p>такс, вроде всё нормально работает</p>
+      <Check />
     </div>
   );
 }
---
+++ src/components/check/check.js
@@ -0,0 +1,11 @@
+import s from './check.module.css';
+
+function Check() {
+  return (
+    <div>
+      <p className={s.styles}>Хочу проверить как работают стили!</p>
+    </div>
+  );
+}
+
+export default Check;
