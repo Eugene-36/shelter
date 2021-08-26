9879a43b36811b784d001debff4089a0cf9c1f06
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-08-26 14:51:38 +0300

    new-updates

--- src/components/check/check.js
+++ src/components/check/check.js
@@ -3,7 +3,7 @@ import s from './check.module.css';
 function Check() {
   return (
     <div>
-      <p className={s.styles}>Хочу проверить как работают стили!</p>
+      <p className={s.styles}>Хочу проверить как работают стили! Всё работет с нового компа</p>
     </div>
   );
 }
