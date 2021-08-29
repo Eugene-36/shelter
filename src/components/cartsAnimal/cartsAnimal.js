f6ea56e9c745b4722a560200ffff162eb65bae3e
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-08-29 18:03:28 +0300

    mobile firts

--- src/components/cartsAnimal/cartsAnimal.js
+++ src/components/cartsAnimal/cartsAnimal.js
@@ -1,17 +1,32 @@
+import React from 'react';
+import s from './carts.module.css';
+import katrine from '../icons/pets-katrine.png';
+import left from '../icons/arrow-left (2).svg';
+//import ArrowLeft from '../arrows/arrowLeft';
+import right from '../icons/arrow-right (2).svg';
 function Carts() {
   return (
-    <section>
-      <h2>Our friends who are looking for a house</h2>
-      <ul>
+    <section className={s.back}>
+      <h2 className={s.headText}>Our friends who are looking for a house</h2>
+      <ul className={s.list}>
         <li>
-          {/* <picture>
-    <img src="" alt="" />
-</picture> */}
-          <h3 class="section-title-info">Katrine</h3>
-          <button>Learn more</button>
-          <button>Left</button>
-          <button>Right</button>
-          <button>Get to know the rest</button>
+          <div className={s.cart}>
+            <img src={katrine} alt="" />
+            <h3 className={s.sectionTitleInfo}>Katrine</h3>
+            <button className={s.bt}>
+              <span className={s.text}>Learn more</span>{' '}
+            </button>
+          </div>
+          <div className={s.posBtn}>
+            {/* <ArrowLeft /> */}
+            <div className={s.circle}>
+              <img src={left} alt="" className={s.arrow} />
+            </div>
+            <div className={s.circle}>
+              <img src={right} alt="" className={s.arrow}/>
+            </div>
+          </div>
+          <button className={s.getToknow}>Get to know the rest</button>
         </li>
       </ul>
     </section>
--- src/components/help/help.js
+++ src/components/help/help.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import s from './help.module.css';
 function Help() {
   return (
     <>
-      <li>Pet food</li>
-      <li>Transportation</li>
-      <li>Toys</li>
-      <li>Bowls and cups</li>
-      <li>Shampoos</li>
-      <li>Vitamins</li>
-      <li>Medicines</li>
-      <li>Collars / leashes</li>
-      <li>Sleeping areas</li>
+      <section>
+        <h2 className={s.textHelp}>How you can help our shelter</h2>
+        <ul className={s.list}>
+          <li>Pet food</li>
+          <li>Transportation</li>
+          <li>Toys</li>
+          <li>Bowls and cups</li>
+          <li>Shampoos</li>
+          <li>Vitamins</li>
+          <li>Medicines</li>
+          <li>Collars / leashes</li>
+          <li>Sleeping areas</li>
+        </ul>
+      </section>
     </>
   );
 }
