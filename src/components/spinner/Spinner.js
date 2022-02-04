515afb50aebe68b0e842a6f6d54c33afec91aed3
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-02-04 17:59:26 +0200

    added new loader

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import s from './spinner.module.css';
+import classNames from 'classnames';
 
-export default function Spinner(props) {
+export default function Spinner() {
   return (
-    <div className={s.box}>
-      <div className={s.plane}></div>
+    <div className={s.common_container}>
+      <div className={s.loading__ball_container}>
+        <div className={s.loading__ball_animate}>
+          <div className={classNames(s.loading__ball_item, s.first)}></div>
+          <div
+            className={classNames(s.loading__ball_shadow, s.first_shadow)}
+          ></div>
+        </div>
+        <div className={s.loading__ball_item}></div>
+        <div className={s.loading__ball_item}></div>
+        <div className={s.loading__ball_item}></div>
+        <div className={s.loading__ball_item}></div>
+        <div className={s.loading__ball_item}></div>
+        <div className={s.loading__ball_animate}>
+          <div className={classNames(s.loading__ball_item, s.last)}></div>
+          <div
+            className={classNames(s.loading__ball_shadow, s.last_shadow)}
+          ></div>
+        </div>
+      </div>
+      <div className={s.loading__text}>
+        <span>Loading...</span>
+      </div>
     </div>
   );
 }
